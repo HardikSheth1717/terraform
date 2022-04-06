03b721e41ee7c1f9136f23dfa649834a58db30fb
Hardik Sheth <hardik.sheth1717@live.com>
2022-04-06 22:32:10 +0530

    Added tutorial for docker nginx and aws-ec2-basic

---
+++ aws-ec2-basic/main.tf
@@ -0,0 +1,24 @@
+terraform {
+  required_providers {
+    aws = {
+      source  = "hashicorp/aws",
+      version = "~> 3.27"
+    }
+  }
+
+  required_version = ">= 0.14.9"
+}
+
+provider "aws" {
+  region  = "ap-south-1"
+  profile = "cloud-desire02"
+}
+
+resource "aws_instance" "learning_instance" {
+  ami           = "ami-0851b76e8b1bce90b"
+  instance_type = "t2.micro"
+
+  tags = {
+    "Name" = "EC2FromTerraform"
+  }
+}
