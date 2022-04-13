044c42862f91ebd82544fba3994b135631f2f58e
Hardik Sheth <hardik.sheth1717@live.com>
2022-04-13 23:03:08 +0530

    Updated main.tf for Azure

--- azure-resource-group/main.tf
+++ azure-resource-group/main.tf
@@ -18,4 +18,15 @@ provider "azurerm" {
 resource "azurerm_resource_group" "my_group" {
   name     = "my-tf-rg"
   location = "westindia"
+  tags = {
+    "Environment" = "Terraform Getting Started"
+    "Team"        = "DevOps"
+  }
+}
+
+resource "azurerm_virtual_network" "vnet" {
+  name                = "my-tf-vnet"
+  address_space       = ["10.0.0.0/16"]
+  location            = "westindia"
+  resource_group_name = azurerm_resource_group.my_group.name
 }
